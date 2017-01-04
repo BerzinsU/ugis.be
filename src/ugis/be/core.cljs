3be82f99f5ef2b077937722eff780b838bfe1823
Ugis Berzins <berzinsu@gmail.com>
2017-01-04 22:55:22 +0200

    migrated to reagent

---
+++ src/ugis/be/core.cljs
@@ -0,0 +1,18 @@
+(ns ugis.be.core
+    (:require [reagent.core :as reagent]))
+
+;; -------------------------
+;; Views
+
+(defn home-page []
+  [:div [:h2 "Under construction, again"]
+   [:h3 "Currently migrating to cljs Reagent from plain HTML"]])
+
+;; -------------------------
+;; Initialize app
+
+(defn mount-root []
+  (reagent/render [home-page] (.getElementById js/document "app")))
+
+(defn init! []
+  (mount-root))
